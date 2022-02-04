1b037767037818a9ad132f0fafec1614a841818b
axlgomez <95052628+axlgomez@users.noreply.github.com>
2022-02-04 13:04:30 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ button.addEventListener("click", (e)=>{
 
     const email = emailAdress.value.trim();
 
-    const emailRegex =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const emailRegex = /^\S+@\S+\.\S+$/;
 
     if(email === '' || !emailRegex.test(email)){
         invalidEmail();
